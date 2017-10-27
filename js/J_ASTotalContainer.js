a0052cf2cc61122040bae2ebfc39ddcad05d9e28
gaoyagit <554800419@qq.com>
2017-10-27 23:39:49 +0800

    最新一次提交

--- js/J_ASTotalContainer.js
+++ js/J_ASTotalContainer.js
@@ -1,19 +1,28 @@
-/**
- * Created by Administrator on 2017/10/27.
- */
-//function getTop(){
-//    var Topnav = document.getElementById('J_ASTotalContainer');
-//    var topMax = window.innerHeight;//��ҳ��Ļ���ø߶�
-//    var scrollTop = document.documentElement.scrollHeight;//�������ĸ߶�
+// function getTop(){
+//     var Topnav = document.getElementById('J_ASTotalContainer');
+//    var topMax = window.innerHeight;
+//    var scrollTop = document.documentElement.scrollTop;
 //    if(scrollTop > topMax){
 //        Topnav.style.display = "block";
 //    }else{
 //        Topnav.style.display = "none";
 //    }
-//}
+// }
+// function addEventHandler(target,type,func) {
+//     if(target.addEventListener()){
+//         target.addEventListener(type, func, false);
+//     }else if(window.attachEvent()){
+//         target.attachEvent("on" + type, func);
+//     }else{
+//         target["on" + type] = func;
+//     }
+// }
+// window.onload = function () {
+//     addEventHandler(window,'scroll',getTop());
+// }
 // $(window).scroll(function(){
 //     var Topnav = document.getElementById('J_ASTotalContainer');
-//     var topMax = window.innerHeight;//��ҳ��Ļ���ø߶�
+//     var topMax = window.innerHeight;
 //     var scrollTop = $(window).scrollTop();
 //     if(scrollTop > topMax){
 //         Topnav.style.display = "block";
@@ -21,7 +30,7 @@
 //         Topnav.style.display = "none";
 //     }
 // })
-//这个也可�?
+//这个也可�?
 // window.onscroll = function(){
 //     var Topnav = document.getElementById('J_ASTotalContainer');
 //     var topMax = window.innerHeight;//网页屏幕可用高度
@@ -42,3 +51,5 @@ window.addEventListener('scroll',function(){
         Topnav.style.display = "none";
     }
 })
+
+
