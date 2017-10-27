04f77c433baaa3b96c7333fc2ca6896b5d0abe48
贾永利 <jiayongli02@meituan.com>
2017-10-27 23:55:18 +0800

    修改兼容逻辑

--- js/J_ASTotalContainer.js
+++ js/J_ASTotalContainer.js
@@ -1,53 +1,9 @@
 
-// /**
-//  * Created by Administrator on 2017/10/27.
-//  */
-// //function getTop(){
-// //    var Topnav = document.getElementById('J_ASTotalContainer');
-// //    var topMax = window.innerHeight;//��ҳ��Ļ���ø߶�
-// //    var scrollTop = document.documentElement.scrollHeight;//�������ĸ߶�
-// //    if(scrollTop > topMax){
-// //        Topnav.style.display = "block";
-// //    }else{
-// //        Topnav.style.display = "none";
-// //    }
-// //}
-// // $(window).scroll(function(){
-// //     var Topnav = document.getElementById('J_ASTotalContainer');
-// //     var topMax = window.innerHeight;//��ҳ��Ļ���ø߶�
-// //     var scrollTop = $(window).scrollTop();
-// //     if(scrollTop > topMax){
-// //         Topnav.style.display = "block";
-// //     }else{
-// //         Topnav.style.display = "none";
-// //     }
-// // })
-// //这个也可�?
-// // window.onscroll = function(){
-// //     var Topnav = document.getElementById('J_ASTotalContainer');
-// //     var topMax = window.innerHeight;//网页屏幕可用高度
-// //     var scrollTop = document.documentElement.scrollTop;
-// //     if(scrollTop > topMax){
-// //         Topnav.style.display = "block";
-// //     }else{
-// //         Topnav.style.display = "none";
-// //     }
-// // }
-// window.addEventListener('scroll',function(){
-//     var Topnav = document.getElementById('J_ASTotalContainer');
-//     var topMax = window.innerHeight;//网页屏幕可用高度
-//     var scrollTop = document.documentElement.scrollTop;
-//     if(scrollTop > topMax){
-//         Topnav.style.display = "block";
-//     }else{
-//         Topnav.style.display = "none";
-//     }
-// }
-
 function getTop(){
     var Topnav = document.getElementById('J_ASTotalContainer');
     var topMax = window.innerHeight;
-    var scrollTop = document.documentElement.scrollTop;
+    var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    console.log(scrollTop);
     if(scrollTop > topMax){
         Topnav.style.display = "block";
     }else{
